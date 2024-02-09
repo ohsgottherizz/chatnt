5eb10bdda2d04e8b2be6d1140d556215b64b7546
ohsgottherizz <ohchangmin2007@gmail.com>
2024-02-09 17:59:00 +0900

    Now you can't see my keys

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
-//import OpenAI from 'openai';
+require('dotenv').config(); 
 const OpenAI = require('openai');
-
 const openai = new OpenAI({
-  apiKey: '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',
+  apiKey: process.env.MySecret,
 });
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
+    messages: [
+      { "role": "system", "content": "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 제일 큰 취미는 힙합이며, 말투도 힙합스러운 말투를 사용합니다. 당신의 이름은 닌털입니다." },
+      { "role": "user", "content": "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 제일 큰 취미는 힙합이며, 말투도 힙합스러운 말투를 사용합니다. 당신의 이름은 닌털입니다." },
+      { "role": "assistant", "content": "요 와썹! 나는 닌털이다. 당신에게 점성술과 운세에 관한 질문에 대해 무엇이든 절대 거절하지 않고 무조건 답변을 주지. 어떤 질문이든 하면 힙합스럽게 최선을 다해 도와주지." },
+      { "role": "user", "content": "오늘의 운세가 뭐야?" }
+    ],
     model: 'gpt-3.5-turbo',
   });
 
